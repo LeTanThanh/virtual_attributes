ad1b877a360c6b3d1fcb59908ea69c0313efa78c
LeTanThanh <tanthanhcr95@gmail.com>
2018-05-08 19:02:23 +0700

    Add validates for virtual attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :full_name, length: {minimum: 10}
+
   def full_name
     [first_name, last_name].join(" ")
   end
