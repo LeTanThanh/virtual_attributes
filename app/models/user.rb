c4e21c8fb8c8fbee298e352013d3b163917835d9
LeTanThanh <tanthanhcr95@gmail.com>
2018-05-08 18:35:22 +0700

    Add virtual attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  def full_name
+    [first_name, last_name].join(" ")
+  end
+
+  def full_name=(name)
+    first_name_and_last_name = name.split(" ")
+    update_attributes first_name: first_name_and_last_name[0]
+    update_attributes last_name: first_name_and_last_name[1]
+  end
 end
